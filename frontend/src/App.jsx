0a99a66acfd1ed72b6fd65bc786ac7a2cfb1c5fc
Caio Batinga <cfrb2008@gmail.com>
2024-11-23 19:42:45 -0500

    app.jsx mod

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -18,22 +18,12 @@ function RegisterAndLogout(){
 
 function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route
-          path="/"
-          element={
-            <ProtectedRoute>
-              <Home />
-            </ProtectedRoute>
-          }
-        />
-        <Route path="/login" element={<Login />} />
-        <Route path="/logout" element={<Logout />} />
-        <Route path="/register" element={<RegisterAndLogout />} />
-        <Route path="*" element={<NotFound />}></Route>
-      </Routes>
-    </BrowserRouter>
+    <Routes>
+    <Route path="/" element={<ProtectedRoute><Home /></ProtectedRoute>} />
+    <Route path="/login" element={<Login />} />
+    <Route path="/register" element={<RegisterAndLogout />} />
+    <Route path="*" element={<NotFound />} />
+  </Routes>
   )
 }
 
