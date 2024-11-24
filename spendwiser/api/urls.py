ac4d673cffdeac23f45be51966fad2834f6e8760
Caio Batinga <cfrb2008@gmail.com>
2024-11-23 20:28:32 -0500

    api

---
+++ spendwiser/api/urls.py
@@ -0,0 +1,7 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path("expenses/", views.ExpenseListCreate.as_view(), name="expense-list"),
+    path("expense/delete/", views.ExpenseDelete.as_view(), name="delete-expense",),
+]
