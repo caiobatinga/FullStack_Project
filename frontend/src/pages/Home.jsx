fef7bbbad42130b36cbbbc069dd5757e0a44d709
Caio Batinga <cfrb2008@gmail.com>
2024-11-24 18:36:02 -0500

    styling 5

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -2,7 +2,7 @@ import { useState, useEffect } from "react"
 import api from "../api"
 import Expense from "../components/Expense"
 import "../styles/Home.css"
-import LoadingIndicator from "./LoadingIndicator";
+import LoadingIndicator from "../components/LoadingIndicator";
 
 
 function Home() {
