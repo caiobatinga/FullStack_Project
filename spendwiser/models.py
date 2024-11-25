40bc8a0baf79949576bf86c54dc169adb90ead35
Caio Batinga <cfrb2008@gmail.com>
2024-11-24 19:32:46 -0500

    Budget 2

--- spendwiser/models.py
+++ spendwiser/models.py
@@ -22,7 +22,7 @@ class Budget(models.Model):
     title = models.CharField(max_length=100)
     amount = models.DecimalField(max_digits=10, decimal_places=2)
     date = models.DateField()
-    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name="user_expenses")
+    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name="user_budgets")
 
     def __str__(self):
         return self.title
