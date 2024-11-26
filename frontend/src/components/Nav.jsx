16f2947c833ea63d3c922d93b90f406813b8929e
Caio Batinga <cfrb2008@gmail.com>
2024-11-25 19:37:35 -0500

    Nav

--- frontend/src/components/Nav.jsx
+++ frontend/src/components/Nav.jsx
@@ -1,28 +1,21 @@
-import React, { useState, useEffect } from "react";
-import api from "../api"; // Ensure your API utility is properly imported.
+import logomark from "../assets/logomark.svg"
+import { Form, NavLink } from "react-router-dom"
 
 const Nav = () => {
-    const [username, setUsername] = useState("");
-
-    useEffect(() => {
-        getUserInfo();
-    }, []);
-
-    const getUserInfo = async () => {
-        try {
-            const response = await api.get("/api/token"); // Replace with the correct endpoint.
-            setUsername(response.data.username);
-        } catch (error) {
-            console.error("Failed to fetch user info", error);
-            alert("Failed to fetch user info");
-        }
-    };
-
     return (
         <nav>
-            <p>Welcome, {username}!</p>
+            <NavLink to="/" aria-label="Home">
+            <img src={logomark} alt="Home" height={30} />
+            <span>SpendWiser</span>
+            </NavLink>
+            <Form
+            method="post"
+            action="/logout">
+                <button type="submit" className="btn btn--warning">
+                <Span>Logout</Span>
+                </button>
+            </Form>
         </nav>
-    );
-};
+    )
 
-export default Nav;
+}
