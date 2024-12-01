43bb94ca96dd76af4000fe57e21503e67c0a951f
Caio Batinga <cfrb2008@gmail.com>
2024-12-01 13:50:22 -0500

    Proxy Design

---
+++ frontend/src/components/AuthProxy.js
@@ -0,0 +1,43 @@
+import jwtDecode from "jwt-decode";
+import api from "../api";
+import { REFRESH_TOKEN, ACCESS_TOKEN } from "../constants";
+
+class AuthProxy {
+    constructor() {
+        this.accessToken = localStorage.getItem(ACCESS_TOKEN);
+        this.refreshToken = localStorage.getItem(REFRESH_TOKEN);
+    }
+
+    async isAuthorized() {
+        if (!this.accessToken) {
+            return false;
+        }
+        const decoded = jwtDecode(this.accessToken);
+        const tokenExpiration = decoded.exp;
+        const now = Date.now() / 1000;
+
+        if (tokenExpiration < now) {
+            return await this.refreshTokenHandler();
+        }
+        return true;
+    }
+
+    async refreshTokenHandler() {
+        if (!this.refreshToken) {
+            return false;
+        }
+        try {
+            const res = await api.post("/api/token/refresh/", {
+                refresh: this.refreshToken,
+            });
+            if (res.status === 200) {
+                this.accessToken = res.data.access;
+                localStorage.setItem(ACCESS_TOKEN, res.data.access);
+                return true;
+            }
+        } catch (error) {
+            console.error("Failed to refresh token:", error);
+        }
+        return false;
+    }
+}
